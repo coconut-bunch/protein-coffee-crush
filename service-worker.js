bc5b64a8e92ae7a7ad60470fb24397ac46ad842a
r3dsm0k3 <ajithredsmoke@gmail.com>
2026-06-05 12:54:10 +0200

    Refresh PWA cache for security update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "protein-coffee-crush-v2";
+const CACHE_NAME = "protein-coffee-crush-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
